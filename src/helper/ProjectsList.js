4a19a082f1b3dc71e31099d95f3978e87d8a3447
Nowruz Owliyagulyyew <161121707+Novruz3@users.noreply.github.com>
2024-07-27 20:29:53 +0500

    added projects

--- src/helper/ProjectsList.js
+++ src/helper/ProjectsList.js
@@ -1,82 +1,81 @@
-import commerce from "../img/projects/commerce.jpg"
-import global_shop from "../img/projects/Global shop.jpg"
-import kitaphana from "../img/projects/kitaphana.jpg"
-import linkedin from "../img/projects/linkedIn.jpg"
-import phone_store from "../img/projects/phone store.jpg"
-import resume from "../img/projects/resume.jpg"
-import send_file from "../img/projects/send file.jpg"
-import turkmenportal from "../img/projects/turkmenportal.jpg"
-import weather from "../img/projects/weather.jpg"
-import youtube from "../img/projects/youtube.jpg"
-import res_ui from "../img/projects/res ui.jpg"
+import commerce from "../img/projects/commerce.jpg";
+import linkedin from "../img/projects/linkedIn.jpg";
+import resume from "../img/projects/resume.jpg";
+import turkmenportal from "../img/projects/turkmenportal.jpg";
+import weather from "../img/projects/weather.jpg";
+import youtube from "../img/projects/youtube.jpg";
+import res_ui from "../img/projects/res ui.jpg";
+import lybas from "../img/projects/lybas.png";
+import lybas_admin from "../img/projects/lybas lybas admin.png";
+import camel_hill from "../img/projects/camell hill.png";
 
 const projects = [
-    {
-        title : 'Resume',
-        skills : 'Html, Css, Javascript, React',
-        img : resume,
-        gitHubLink : 'https://github.com/Novruz3/Resume'
-    },
-    {
-        title : 'Kitaphana clone',
-        skills : 'Html, Css, Javascript, Bootstrap, React',
-        img : kitaphana,
-        gitHubLink : 'https://github.com/Novruz3/kitaphana_clone'
-    },
-    {
-        title : 'Phone store shopping',
-        skills : 'Html, Css, Javascript, Bootstrap, React, Context',
-        img : phone_store,
-        gitHubLink : 'https://github.com/Novruz3/Phone_store_shopping'
-    },
-    {
-        title : 'Turkmenportal clone',
-        skills : 'Html, Css, Javascript, Bootstrap, React',
-        img: turkmenportal,
-        gitHubLink : 'https://github.com/Novruz3/Turkmenportal_clone'
-    },
-    {
-        title : 'Weather app',
-        skills : 'Html, Css, Javascript, Bootstrap, React, Axios, Api',
-        img: weather,
-        gitHubLink : 'https://github.com/Novruz3/Weather_app'
-    },
-    {
-        title : 'LinkedIn sign in with firebase',
-        skills : 'Html, Css, Javascript, React, Material UI, Api, Axios, Firebase',
-        img: linkedin,
-        gitHubLink : 'https://github.com/LinkedIn_sign_in_with_firebase'
-    },
-    {
-        title : 'Online send file',
-        skills : 'Html, Css, Javascript, Bootstrap, React, Zustand',
-        img: send_file,
-        gitHubLink : 'https://github.com'
-    },
-    {
-        title : 'E-commerce',
-        skills : 'Html, Css, Javascript, React, Typescript, Tailwind Css, Zustand, Axios, Api',
-        img: commerce,
-        gitHubLink : 'https://github.com/Novruz3/react-typescript-taillwind-zustand'
-    },
-    {
-        title : 'Global shop',
-        skills : 'Html, Css, Javascript, React, Typescript, Tailwind Css, Axios, Api, Redux Toolkit',
-        img: global_shop,
-        gitHubLink : 'https://github.com/Novruz3/GLOBAL-SHOP-online-store'
-    },
-    {
-        title : 'Youtube clone',
-        skills : 'Html, Css, Javascript, React, Typescript, Tailwind Css, Axios, Api, Redux Toolkit',
-        img: youtube,
-        gitHubLink : 'https://github.com/Novruz3/youtube-clone'
-    },
-    {
-        title : 'Online order foods',
-        skills : 'Html, Css, Javascript, React, Next JS, Typescript, Tailwind Css, Zustand',
-        img: res_ui,
-        gitHubLink : 'https://github.com/Novruz3/restaurant_ui/tree/master/restaurant_ui'
-    }
-]
+  {
+    title: "Lybas",
+    skills:
+      "Html, Css, Javascript, React, Tailwind Css, Redux Toolkit, Axios, Api",
+    img: lybas,
+    gitHubLink: "https://github.com/Novruz3/lybas-e-commerce",
+  },
+  {
+    title: "Lybas admin",
+    skills:
+      "Html, Css, Javascript, React, Tailwind Css, Redux Toolkit, Axios, Api",
+    img: lybas_admin,
+    gitHubLink: "https://github.com/Novruz3/lybas-admin",
+  },
+  {
+    title: "Camell Hill",
+    skills:
+      "Html, Css, Javascript, React, Tailwind Css, Redux Toolkit, Axios, Api, Material UI",
+    img: camel_hill,
+    gitHubLink: "https://github.com/Novruz3/Camel-Hill",
+  },
+  {
+    title: "Resume",
+    skills: "Html, Css, Javascript, React",
+    img: resume,
+    gitHubLink: "https://github.com/Novruz3/Resume",
+  },
+  {
+    title: "Turkmenportal clone",
+    skills: "Html, Css, Javascript, Bootstrap, React",
+    img: turkmenportal,
+    gitHubLink: "https://github.com/Novruz3/Turkmenportal_clone",
+  },
+  {
+    title: "Weather app",
+    skills: "Html, Css, Javascript, Bootstrap, React, Axios, Api",
+    img: weather,
+  },
+  {
+    title: "LinkedIn sign in with firebase",
+    skills: "Html, Css, Javascript, React, Material UI, Api, Axios, Firebase",
+    img: linkedin,
+    gitHubLink: "https://github.com/LinkedIn_sign_in_with_firebase",
+  },
+  {
+    title: "E-commerce",
+    skills:
+      "Html, Css, Javascript, React, Typescript, Tailwind Css, Zustand, Axios, Api",
+    img: commerce,
+    gitHubLink: "https://github.com/Novruz3/react-typescript-taillwind-zustand",
+  },
+  {
+    title: "Youtube clone",
+    skills:
+      "Html, Css, Javascript, React, Typescript, Tailwind Css, Axios, Api, Redux Toolkit",
+    img: youtube,
+    gitHubLink: "https://github.com/Novruz3/youtube-clone",
+  },
+  {
+    title: "Online order foods",
+    skills:
+      "Html, Css, Javascript, React, Next JS, Typescript, Tailwind Css, Zustand",
+    img: res_ui,
+    gitHubLink:
+      "https://github.com/Novruz3/restaurant_ui/tree/master/restaurant_ui",
+  },
+];
 
-export {projects}
+export { projects };
