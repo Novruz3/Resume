1f9616a98f5ef761d96a26c70633f4d3ef0ceab1
Novruz <owliyagulyyewnowruz@gmail.com>
2024-07-28 19:20:39 +0500

    add experience

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,12 +9,18 @@ export const Home = () => {
                 <div className="container">
                     <ul className="content-list">
                         <li className="content-list__item">
-                            <p className="title-2">Skills</p>
+                            <p style={{color : "blue"}} className="title-2">Skills</p>
                             <p className='skills__item'>HTML, CSS, Javascript, Bootstrap, React JS, Next JS, Typescript, Tailwind CSS, Redux Toolkit, Zustand, Github, Axios, Rest API</p>
                         </li>
                         <li className="content-list__item">
-                            <p className="title-2">Languages</p>
+                            <p style={{color : "blue"}} className="title-2">Languages</p>
                             <p className='skills__item'>Turkmen, Russian, English</p>
+                        </li>
+                        <li className="content-list__item">
+                            <p style={{color : "blue"}} className="title-2">Experience</p>
+                            <p className='skills__item__2'>Geek Space </p>
+                            <p style={{fontSize:"28px", fontWeight:'600'}} className='skills__item'>Frontend Developer </p>
+                            <p style={{fontSize: "24px"}} className='skills__item'>February 2024 - Present</p>
                         </li>
                     </ul>
                 </div>
